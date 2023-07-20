007cdef36a8656babc0b51307a3d76f752413e92
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-21 02:46:00 +0300

    backend: bug fix in login.php

--- backend/login.php
+++ backend/login.php
@@ -7,13 +7,13 @@
     $response = [];
     if(isset($data["email"]) && $data["email"] != ""){
         $email = trim($data["email"]);
-        $password_user = trim($data["password"]);
+        $password = trim($data["password"]);
         $query = $conn -> prepare("SELECT * FROM users WHERE email = ?");
         $query -> bind_param("s", $email);
         $query->execute();
 
         $query->store_result();
-        $query->bind_result($id, $email, $password, $name);
+        $query->bind_result($id, $email, $hashed_password, $name);
         $query->fetch();
 
         $num_rows = $query->num_rows();
@@ -21,7 +21,7 @@
             $response["success"] = false;
             $response['status'] = "user not found";
         } else {
-            if ($password == $password_user) {
+            if (password_verify($password,$hashed_password)) {
                 $response["success"] = true;
                 $response['status'] = 'logged in';
                 $response['user_id'] = $id;

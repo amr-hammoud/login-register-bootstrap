7ab89df4e9b6a6a2b00a5cf49d61e71faab99425
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-21 02:43:43 +0300

    backend: bug fix in register.php

--- backend/register.php
+++ backend/register.php
@@ -2,7 +2,7 @@
     include ("connection.php");
 
     header("Content-Type: application/json");
-    $data = json_decode(file_get_contents('php://input'), true);
+    $data = json_decode(file_get_contents("php://input"), true);
 
     $response = [];
     if(isset($data["email"]) && $data["email"] != ""){
@@ -10,23 +10,23 @@
         $email = trim($data["email"]);
         $password = trim($data["password"]);
 
-        $check_email = $conn->prepare('select email from users where email=?');
-        $check_email->bind_param('s', $email);
+        $check_email = $conn->prepare("select email from users where email=?");
+        $check_email->bind_param("s", $email);
         $check_email->execute();
         $check_email->store_result();
         $user_exists = $check_email->num_rows();
     
         if ($user_exists == 0) {
             $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-            $query = $conn->prepare('insert into users(email,password,name) values(?,?,?)');
-            $query->bind_param('sss', $email, $hashed_password, $name);
+            $query = $conn->prepare("insert into users(email,password,name) values(?,?,?)");
+            $query->bind_param("sss", $email, $hashed_password, $name);
             $query->execute();
     
-            $response['status'] = "success";
-            $response['message'] = "another message in success";
+            $response["success"] = true;
+            $response["message"] = "another message in success";
         } else {
-            $response['status'] = "failed";
-            $response['message'] = "another message in fail";
+            $response["success"] = false;
+            $response["message"] = "another message in fail";
         }
     }
     else{

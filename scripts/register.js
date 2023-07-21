825de8b90e71ce981a850a367ea6b26c19b7c9cc
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-21 03:05:25 +0300

    frontend: add registration input validation

--- scripts/register.js
+++ scripts/register.js
@@ -10,34 +10,56 @@ function registerUser(e) {
 	const password_text = document.getElementById("register-password").value.trim();
 	const confirm_password_text = document.getElementById("register-confirm-password").value.trim();
 
-    if(password_text === confirm_password_text){
-        const register_info = {
-            name: name_text,
-            email: email_text,
-            password: password_text,
-        };
-    
-        const register_url = base_url + "register.php"
-        fetch(register_url, {
-            method: "POST",
-            headers:{
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(register_info),
-        })
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                if (json["success"] === true) {
-                    localStorage.setItem("name", json["name"]);
-                    window.location.href = "index.html"
-                } else {
-                    console.log(json["message"]);
-                }
+    if(checkInput(name_text,email_text,password_text,confirm_password_text)){
+            const register_info = {
+                name: name_text,
+                email: email_text,
+                password: password_text,
+            };
+        
+            const register_url = base_url + "register.php"
+            fetch(register_url, {
+                method: "POST",
+                headers:{
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(register_info),
             })
-            .catch((error) => console.log(error));
+                .then((response) => response.json())
+                .then((json) => {
+                    console.log(json);
+                    if (json["success"] === true) {
+                        localStorage.setItem("name", json["name"]);
+                        window.location.href = "index.html"
+                    } else {
+                        console.log(json["message"]);
+                    }
+                })
+                .catch((error) => console.log(error));
+        
     }
 
-	
+}
+
+function checkInput(name, email, password, confirm_password){
+    let name_valid = false
+    let email_valid = false
+    let password_valid = false
+
+    if(name != "" && email != "" && password != "" && confirm_password != ""){
+        const nameregex = /^[A-Za-z\s]*$/
+        const emailregex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+        if(nameregex.test(name))
+            name_valid = true
+        
+        if(emailregex.test(email))
+            email_valid = true
+        
+        }
+        if (password.length() >=8 && password === confirm_password){
+            password_valid = true
+        }
+    
+    return name_valid && email_valid && password_valid
 }
